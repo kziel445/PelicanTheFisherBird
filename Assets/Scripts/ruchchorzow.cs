a0ade971d2bc13866addbb4a9961052958f730a1
Kardiologpythona <91539787+Kardiologpythona@users.noreply.github.com>
2022-05-10 20:02:55 +0200

    Add zajebisty ruch Pelikana

--- Assets/Scripts/ruchchorzow.cs
+++ Assets/Scripts/ruchchorzow.cs
@@ -2,23 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ruchchorzow : MonoBehaviour
+    
 {
+    public Rigidbody2D rg;
+    public float silaskosku = 5f;
     [SerializeField] float jumpModificator = 2;
+    void Start()
+    {
+        rg = GetComponent<Rigidbody2D>();  
+    }
 
     void Update()
     {
         if (Input.touchCount > 0)
         {
-            transform.Translate(0, jumpModificator * Time.deltaTime, 0, Space.World);
+            var touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                Skok();
+            }
         }
         else
         {
-            transform.Translate(0, -1 * Time.deltaTime, 0, Space.World);
+            
         }
         if(Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(-1f * Time.deltaTime , 0, 0, Space.World);
         }
+    }
+    void Skok()
+    {
+        rg.velocity = Vector2.zero;
+        rg.AddForce(new Vector2(0, silaskosku), ForceMode2D.Impulse);
     }
 }
