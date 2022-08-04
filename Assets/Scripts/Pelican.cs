95507631f3af248856c9da23b55923da4f8611a3
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2022-08-04 21:05:16 +0200

    pelican fly concept

--- Assets/Scripts/Pelican.cs
+++ Assets/Scripts/Pelican.cs
@@ -15,6 +15,7 @@ public class Pelican : MonoBehaviour
 
     public Rigidbody2D rg;
     public Animator animator;
+    public Animator wingAnimator;
     public float silaskosku = 5f;
     private Collider2D collider;
     private State state;
@@ -40,6 +41,8 @@ public class Pelican : MonoBehaviour
         rg.bodyType = RigidbodyType2D.Static;
 
         animator = GetComponent<Animator>();
+        wingAnimator = transform.GetChild(0).GetComponent<Animator>();
+
         collider = GetComponent<BoxCollider2D>();
         collider.enabled = false;
     }
@@ -98,11 +101,13 @@ public class Pelican : MonoBehaviour
     {
         rg.velocity = Vector2.zero;
         rg.AddForce(new Vector2(0, silaskosku), ForceMode2D.Impulse);
+        wingAnimator.Play("Fly");
     }
     void Catch()
     {
         collider.enabled = true;
-        animator.Play("PelicanCatching");
+        animator.Play("PelicanCatching",-1,0f);
+
     }
     void Die()
     {
