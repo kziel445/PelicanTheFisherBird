1a29911feed067b9abbe719277bf6140ff2b3d09
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2022-09-25 23:19:23 +0200

    more

--- Assets/Scripts/BackgroundScroller.cs
+++ Assets/Scripts/BackgroundScroller.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BackgroundScroller : MonoBehaviour
 {
     [Range(-1f, 1f)]
-    public float ScrollSpeed = 0.5f;
+    public float ScrollSpeed = -0.005f;
     private float offset;
     private Material mat;
 
