1aba8e1b83942d98cac1648a7f018e5499866537
Kamil Zieliński <47740876+kziel445@users.noreply.github.com>
2023-02-08 22:13:16 +0100

    add gameController with water level declaration

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.YamlDotNet.Core.Tokens;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Transform waterHeight;
+    private float waterLevel;
+    private bool waterInitialized = false;
+    public float WaterLevel
+    {
+        get { return waterLevel;}
+        private set 
+        {
+            if (!waterInitialized)
+            {
+                waterLevel = value;
+                waterInitialized = true;
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        WaterLevel = waterHeight.position.y;
+    }
+
+
+}
